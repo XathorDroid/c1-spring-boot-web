cfd0a6762fa8f182cf1cac05090e0aaa3acac047
Xathor <andymarcel.97@gmail.com>
2021-04-01 22:05:40 +0200

    Initial commit.

---
+++ src/main/java/com/xathordroid/c1springbootweb/C1SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.xathordroid.c1springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xathordroid/c1springbootweb/C1SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xathordroid.c1springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

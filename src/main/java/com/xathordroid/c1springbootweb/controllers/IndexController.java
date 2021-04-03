7871f74e86bb76ccd09b48b93951e712f4a1d617
Xathor <andymarcel.97@gmail.com>
2021-04-03 18:51:14 +0200

    Add common RequestMapping for the controller.

--- src/main/java/com/xathordroid/c1springbootweb/controllers/IndexController.java
+++ src/main/java/com/xathordroid/c1springbootweb/controllers/IndexController.java
@@ -3,8 +3,10 @@ package com.xathordroid.c1springbootweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
     
     @GetMapping(value ="/index")
